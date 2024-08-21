458f52267e254216e12b7b62c7027bbd7fa23c4d
Abhishek <abhiiit94524@gmail.com>
2024-08-21 21:29:23 +0530

    https://github.com/Abhishe-kYadav/https---github.com-Abhishe-kYadav-LeetCode

--- leetcode344.java
+++ leetcode344.java
@@ -1,19 +1,19 @@
 import java.util.*;
 public class leetcode344 {
     public static void main(String[] args) {
-        String s[]={"h","e","l","l","o"};
-        System.out.println(Arrays.toString(reverse(s)));
+        char s[]={'h','e','l','l','o'};
+        reverse(s);
+        System.out.println(Arrays.toString(s));
     }
-   static String[] reverse(String s[]){
+   static void reverse(char s[]){
      int start=0;
      int end=s.length-1;
      while(start<end){
-        String temp=s[start];
+        char temp=s[start];
         s[start]=s[end];
         s[end]=temp;
         start++;
         end--;
      }
-     return s;
    }
 }
